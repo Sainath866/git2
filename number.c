ccfc3bd6fc9a40428c2d8cc9982ec22c4d63df09
Sainath866 <tommy99451@gmail.com>
2025-10-20 09:05:00 +0530

    sai

---
+++ number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int *p ,a[10],i, sum,n;
+  //printf("enter the number");
+   scanf("%d",&n);
+   p=a;
+   for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+   }
+   for(i=0;i<n;i++);
+   {
+    sum=sum+*p;
+    p++;
+   }
+printf("sum is %d",sum);
+return 0;
+
+   }
+
